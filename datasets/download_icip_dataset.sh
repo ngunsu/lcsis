42905112518f3393bc1435a64948a02e5104e9c9
Cristhian Aguilera <ngunsu@gmail.com>
2016-09-16 12:07:16 +0200

    Fix missing link

--- datasets/download_icip_dataset.sh
+++ datasets/download_icip_dataset.sh
@@ -1,6 +1,6 @@
 mkdir icip2015
 cd icip2015
-wget http://www.dropbox.com/s/g3cj62gls3s1wqc/icip2015eval.t7.zip?dl=0 -O icip2015.zip
+wget /media/cristhian/Storage01/datasets/icip2015/icip2015eval.t7.zip -O icip2015.zip
 unzip icip2015.zip
 rm icip2015.zip
 
