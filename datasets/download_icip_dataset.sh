73f9be78d9af7d966816544aae7139fcfa136283
Cristhian Aguilera <ngunsu@gmail.com>
2016-09-16 12:08:08 +0200

    Fixes link

--- datasets/download_icip_dataset.sh
+++ datasets/download_icip_dataset.sh
@@ -1,6 +1,6 @@
 mkdir icip2015
 cd icip2015
-wget /media/cristhian/Storage01/datasets/icip2015/icip2015eval.t7.zip -O icip2015.zip
+wget https://www.dropbox.com/s/cg35kecuktxrqjn/icip2015eval.t7.zip?dl=0 -O icip2015.zip
 unzip icip2015.zip
 rm icip2015.zip
 
