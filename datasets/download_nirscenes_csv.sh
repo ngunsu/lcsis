fcd128e46a69ba0dee7bc847b6d26a36238ee993
Cristhian Aguilera <ngunsu@gmail.com>
2016-09-16 11:55:12 +0200

    Fix missing link

--- datasets/download_nirscenes_csv.sh
+++ datasets/download_nirscenes_csv.sh
@@ -1,5 +1,5 @@
 mkdir nirscenes
 cd nirscenes
-wget https://www.dropbox.com/s/hz9s5hoi08tu8dh/csv.zip?dl=0 -O csv.zip
+wget https://www.dropbox.com/s/p034cey0vtcpbba/csv.zip?dl=0 -O csv.zip
 unzip csv.zip
 rm csv.zip
